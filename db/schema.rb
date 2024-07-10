a75e4ebb79df0789df5ac1f122ce47884230401d
Jenna Claudia Andriamanantena <jennaandria@gmail.com>
2024-07-10 15:35:48 +0200

    Installation de geocoder

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_10_072646) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_10_132705) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -113,6 +113,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_10_072646) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "photos"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_vans_on_user_id"
   end
 
