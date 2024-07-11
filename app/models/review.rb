55ef4c51f103f516e468b615a03278ecc1b5067b
Solene Baudic <solene.baudic@gmail.com>
2024-07-11 12:39:56 +0200

    Review mis en place

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,5 +5,4 @@ class Review < ApplicationRecord
   validates :rating, presence: { message: "Ne peut pas être vide" }, inclusion: { in: 1..5, message: "Doit être compris entre 1 et 5" }
   validates :comment, presence: { message: "Ne peut pas être vide" }, length: { maximum: 300, message: "Ne peut pas dépasser 300 caractères" }
   validates :van_id, presence: true
-
 end
